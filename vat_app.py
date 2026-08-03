6420a751e48bd66c6cf35486b2cdc2fb9f36b226
414-01-kwanlada <54153@yupparaj.ac.th>
2026-08-03 14:33:39 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,10 +1,15 @@
- import streamlit as st
-st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-vat = price * 0.07
-net_price = price - vat
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
-st.divider()
-st.write("นางสาวขวัญลดา อุดโน เลขที่ 1  ม.4/14")
+import streamlit as st
 
+st.title("🛒 แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", min_value=0.0, value=0.0)
+
+vat = price * 0.07
+net_price = price + vat
+
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): {vat:.2f} บาท")
+st.header(f"• ราคาสุทธิ (รวม VAT): {net_price:.2f} บาท")
+
+st.divider()
+
+st.write("นางสาวขวัญลดา อุดโน เลขที่ 1 ม.4/14")
